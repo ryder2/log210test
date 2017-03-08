63c775e8541d2c85ec33800480cc27934547e770
Yoann <yoann_gau@hotmail.com>
2017-03-08 17:39:34 -0500

    added user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,6 @@
 Role.create(name: 'Directeur')
 Role.create(name: 'Coordonnateur')
 Role.create(name: 'Intervenant')
+
+connection = ActiveRecord::Base.connection()
+connection.execute("INSERT INTO users VALUES (2,'1@1.com','$2a$11$/L67QyC9XedTzjEuxNRZ/Op.henF/lKxDCm6FzmhuONr.C5p9KI0K',null,null,null,1,null,null,null,null,'2012-12-31 00:00:00','2012-12-31 00:00:00',0,null,null,0,'Yoann')")
