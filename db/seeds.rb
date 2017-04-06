853635602da39694a206fa10ba83d9ffb9791313
Yoann <yoann_gau@hotmail.com>
2017-04-05 22:11:16 -0400

    ajuster le seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Role.create(name: 'Directeur')
-Role.create(name: 'Coordonnateur')
-Role.create(name: 'Intervenant')
+Role.create(id: 0, name: 'Directeur')
+Role.create(id: 1, name: 'Coordonnateur')
+Role.create(id: 2, name: 'Intervenant')
 
 connection = ActiveRecord::Base.connection()
 connection.execute("INSERT INTO users VALUES (2,'1@1.com','$2a$11$/L67QyC9XedTzjEuxNRZ/Op.henF/lKxDCm6FzmhuONr.C5p9KI0K',null,null,null,1,null,null,null,null,'2012-12-31 00:00:00','2012-12-31 00:00:00',0,null,null,0,'Yoann')")
